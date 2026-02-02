b4e4f536db782985bd005b18c9b079d3163b8163
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-02-02 11:20:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,45 @@ import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 import webHookRouter from "./routes/webhook.route.js"
-import { clerkMiddleware } from "@clerk/express"
+import {clerkMiddleware, requireAuth} from '@clerk/express'
 import cors from "cors"
+const app=express()
+app.use(cors(process.env.CLIENT_URL) )
 
-const app = express()
-
-app.use(express.json())
-
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
-}))
-
-app.use("/webhooks", webHookRouter)
-
+app.use("/webhooks",webHookRouter)
 app.use(clerkMiddleware())
+app.use(express.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use("/users", userRouter)
-app.use("/posts", postRouter)
-app.use("/comments", commentRouter)
+// app.get("/test",(req,res)=>{
+//     res.status(200).send("it works!")
+// })
 
-app.use((error, req, res, next) => {
-  res.status(error.status || 500).json({
-    message: error.message || "Something went wrong!",
-    status: error.status,
-    stack: error.stack,
-  })
+// app.get("/auth-state",(req,res)=>{
+//     const authState=req.auth()
+//     res.json(authState)
+// })
+// app.get("/protect2",requireAuth(),(req,res)=>{
+//     res.status(200).json("content")
+// })
+app.use("/users",userRouter) 
+app.use("/posts",postRouter)
+app.use("/comments",commentRouter)
+
+app.use((error,req,res,next)=>{
+    res.status(error.status ||500)
+    res.json({
+        message:error.message ||"Something went wrong!",
+        status:error.status,
+        stack:error.stack,
+    })
 })
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  connectDB()
-  console.log("Server running on", PORT)
+app.listen(3000,()=>{
+    connectDB()
+    console.log("Server is running!")
 })
